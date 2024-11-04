b6e5a1ddd530f7c254c8984627e8e64228dc54dd
Eric C. <eyfchoi@gmail.com>
2024-11-04 23:15:33 +0800

    Content. Completed basic Calculator algorithm.

--- script.js
+++ script.js
@@ -1,9 +1,29 @@
-const calculator  = document.querySelector("#calculator");
-const screen      = document.querySelector("#screen");
-const buttonPanel = document.querySelector("#button-panel");
-
 const keyTextArray = ["AC", "+/-", "%", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1" ,"2", "3", "+", "0", ".", "="];
 const keyIdArray   = ["ac", "plus-minus", "percent", "slash", "seven", "eight", "nine", "asterisk", "four", "five", "six", "minus", "one" ,"two", "three", "plus", "zero", "period", "equal"];
+const calculator   = document.querySelector("#calculator");
+const screen       = document.querySelector("#screen");
+const buttonPanel  = document.querySelector("#button-panel");
+
+let num1 = "";
+let num2 = "";
+let oprt = "";
+
+function operate(n1, n2, op)
+{
+    switch(op)
+    {
+        case "+":
+            return Number(n1) + Number(n2);
+        case "-":
+            return Number(n1) - Number(n2);
+        case "*":
+            return Number(n1) * Number(n2);
+        case "/":
+            return Number(n1) / Number(n2);
+        default:
+            return 0;
+    }
+}
 
 for (let i = 0; i < keyTextArray.length; i++)
 {
@@ -19,3 +39,43 @@ for (let i = 0; i < keyTextArray.length; i++)
 
     buttonPanel.appendChild(button);   
 }
+
+buttonPanel.addEventListener('click', (e) => {
+    
+    const targ  = e.target;
+    const isNum = targ.classList.contains("number-key");
+    const isOp  = targ.classList.contains("operator-key")
+
+    console.log(targ.id + " " + targ.classList);
+
+    if (!isNum && !isOp) 
+        return;
+
+    if (isNum)
+        if (oprt == "")
+            num1 += targ.textContent;
+        else 
+            num2 += targ.textContent;
+
+    if (isOp)
+        if (targ.id == "equal")
+        {
+            screen.textContent = String(operate(num1, num2, oprt));
+            num1 = "";
+            num2 = "";
+            oprt = "";
+            return;
+        }
+        else if (targ.id == "ac")
+        {
+            num1 = "";
+            num2 = "";
+            oprt = "";
+        }
+        else
+        {
+            oprt += targ.textContent;
+        }
+        
+    screen.textContent = num1 + oprt + num2;
+});
